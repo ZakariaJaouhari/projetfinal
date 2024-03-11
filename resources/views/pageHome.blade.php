9b49200f3f071ea8da7385299d49335470209e51
ZakariaJaouhari <zkrjaouhari05@gmail.com>
2024-03-11 17:29:16 +0000

    first commit

---
+++ resources/views/pageHome.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="pageHome.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="profile">
+            <img src="profile.jpg" alt="Profile picture">
+            <h3>Mike Ross</h3>
+        </div>
+        <ul class="nav">
+            <li id="tophome">
+                <a href="#">
+                    <img class="logos home" src="Bhome.png" alt="">
+                    <img class="logos home hv" src="Bhome-hover.png" alt=""> Home</a>
+            </li>
+            <li>
+                <a href="#">
+                    <img class="logos formateur" src="prof.png" alt="">
+                    <img class="logos formateur hv" src="prof-hover.png" alt="">Formateurs</a>
+            </li>
+            <li>
+                <a href="#">
+                    <img class="logos stagiaire" src="stagiaire.png" alt="">
+                    <img class="logos stagiaire hv" src="stagiaire-hover.png" alt="">Stagiaires</a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos module" src="module.png" alt="">
+                <img class="logos module hv" src="module-hover.png" alt="">Modules</a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos notes" src="note.png" alt="">
+                <img class="logos notes hv" src="note-hover.png" alt=""><span>Notes</span></a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos demande" src="certificat.png" alt="">
+                <img class="logos demande hv" src="certificat-hover.png" alt=""><span>Certificats</span></a>
+            </li>
+            <li id="logout">
+                <a href="#">
+                <img class="logos" src="logout.png" alt="">
+                <img class="logos hv" src="logout-hover.png" alt="">Log out</a>
+            </li>
+        </ul>
+    </div>
+    <div id="calendar"></div>
+   
+
+        
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="inscription.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <img src="{{ asset('logo.svg') }}" alt="Logo"> 
+    <div class="container" id="container">
+        
+        <div class="form-container sign-up">
+            <form>
+                <h1>S’inscrire</h1>
+                <p>C’est rapide et facile.</p>
+                <input type="text" placeholder="Nom d'école">
+                <input type="text" placeholder="Nom complet de directeur">
+                <input type="email" placeholder="Adresse email">
+                <input type="password" placeholder="Mot de passe">
+                <input type="password" placeholder="Confirmer le mot de passe">
+                <button>s'inscrire</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form>
+                <h1>Bienvenue</h1>
+                <input type="email" placeholder="Login">
+                <input type="password" placeholder="Mot de passe">
+                <a href="#">Mot de passe oublié?</a>
+                <button>Connexion</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Bonjour!</h1>
+                    <p>Un système d'information intégré qui permet la mise en place de nouvelles méthodes de gestion scolaire.</p>
+                    <button class="hidden" id="login">se connecter</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Bonjour!</h1>
+                    <p>Un système d'information intégré qui permet la mise en place de nouvelles méthodes de gestion scolaire.</p>
+                    <button class="hidden" id="register">Créer compte pour mon école</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const container = document.getElementById('container');
+        const registerBtn = document.getElementById('register');
+        const loginBtn = document.getElementById('login');
+
+        registerBtn.addEventListener('click', () => {
+            container.classList.add("active");
+        });
+
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+    </script>
+</body>
+
+</html>
