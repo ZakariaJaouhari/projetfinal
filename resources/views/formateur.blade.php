987f9948215f3c512a757b90f1edc90051c278b2
ZakariaJaouhari <zkrjaouhari05@gmail.com>
2024-03-24 01:32:44 +0000

    #

---
+++ resources/views/formateur.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="formateur.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="profile">
+            <img src="profile.jpg" alt="Profile picture">
+            <h3>Mike Ross</h3>
+        </div>
+        <ul class="nav">
+            <li id="tophome">
+                <a href="#">
+                    <img class="logos home" src="Bhome.png" alt="">
+                    <img class="logos home hv" src="Bhome-hover.png" alt=""> Home</a>
+            </li>
+            <li>
+                <a href="#">
+                    <img class="logos formateur" src="prof.png" alt="">
+                    <img class="logos formateur hv" src="prof-hover.png" alt="">Profile</a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos groupes" src="groupe.png" alt="">
+                <img class="logos groupes hv" src="groupe-hover.png" alt="">Groupes</a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos module" src="module.png" alt="">
+                <img class="logos module hv" src="module-hover.png" alt="">Modules</a>
+            </li>
+            <li>
+                <a href="#">
+                <img class="logos notes" src="note.png" alt="">
+                <img class="logos notes hv" src="note-hover.png" alt=""><span>Notes</span></a>
+            </li>
+            <li id="logout">
+                <a href="#">
+                <img class="logos" src="logout.png" alt="">
+                <img class="logos hv" src="logout-hover.png" alt="">Log out</a>
+            </li>
+        </ul>
+    </div>
+    <div id="calendar"></div>
+</body>
+
+</html>
