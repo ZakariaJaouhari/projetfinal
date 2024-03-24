88ad2c61e7d03b3aa82bcc424f18e2e4db28fc61
ZakariaJaouhari <zkrjaouhari05@gmail.com>
2024-03-24 01:32:16 +0000

    #

---
+++ app/Http/Controllers/DirecteurController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewDirecteur;
+use Illuminate\Http\Request;
+
+class DirecteurController extends Controller
+{
+    //
+    public function Ajouter(Request $request)
+    {
+        
+        $validatedData = $request->validate([
+            'nom_ecole' => 'required|string',
+            'nom_directeur' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+            'confirmation_password' => 'required|string|same:password',
+
+
+        ]);
+
+
+    
+        NewDirecteur::create([
+            'nom_ecole' => $validatedData['nom_ecole'],
+            'nom_directeur' => $validatedData['nom_directeur'],
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+            'confirmation_password' => $validatedData['confirmation_password'],
+        ]);
+
+      
+    }
+}
+
